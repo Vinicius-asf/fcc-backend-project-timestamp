d019fa0d0e70a6a573dcd260cfd85ad86eee7041
Vinicius Ferreira <viniciusant3@gmail.com>
2022-05-31 10:22:39 -0300

    chore: adjusting for testing

--- index.js
+++ index.js
@@ -3,26 +3,10 @@
 
 // init project
 var express = require('express');
-var app = express();
 
-const timestamp = require('./src/modules/timestamp')
+const createServer = require('./server');
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-app.use('/api',timestamp)
-
-// listen for requests :)
-var listener = app.listen(3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+const app = createServer();
+app.listen(3000, () => {
+  console.log("Server is listening port 3000")
+})
